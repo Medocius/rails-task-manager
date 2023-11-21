418c232632552f754e7e073d319da11b3b867d7c
Medard Kuklis <medocius@gmail.com>
2023-11-21 16:55:23 +0100

    added create show and update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update", as: "update"
+  get "tasks/:id", to: "tasks#show", as: "task"
+  get "tasks/:id/edit", to: "tasks#edit"
 
   # Defines the root path route ("/")
   # root "posts#index"
